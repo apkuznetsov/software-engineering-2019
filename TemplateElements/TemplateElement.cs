07247607449a63eb9e5b2b124af665c648f36ae4
Andrey <kouznts@ya.ru>
2019-11-29 21:24:19 +0400

    добавить абстрактный класс ШЭ

---
+++ TemplateElements/TemplateElement.cs
@@ -0,0 +1,8 @@
+﻿namespace software_engineering_2019.TemplateElements
+{
+    public abstract class TemplateElement
+    {
+        public abstract string Name { get; set; }
+        public abstract string ImageLocation { get; set; }
+    }
+}
