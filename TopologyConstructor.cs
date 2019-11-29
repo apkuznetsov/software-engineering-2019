558105fecd92a63dd9d87fe2be641666ce560884
Andrey <kouznts@ya.ru>
2019-11-29 20:09:42 +0400

    вынести установку настроек конструктора топологии в отдельный файл

--- TopologyConstructor.cs
+++ TopologyConstructor.cs
@@ -19,55 +19,6 @@ namespace software_engineering_2019
             SetSettings();
         }
 
-        private void SetSettings()
-        {
-            SetFillingStationField();
-            SetCellsSize();
-
-            SetSpinners();
-        }
-
-        private void SetFillingStationField()
-        {
-            fillingStationField.RowHeadersVisible = false;
-            fillingStationField.ColumnHeadersVisible = false;
-
-            fillingStationField.AllowUserToResizeColumns = false;
-            fillingStationField.AllowUserToResizeRows = false;
-
-            fillingStationField.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            fillingStationField.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
-
-            fillingStationField.RowCount = Settings.MinNumOfCellsVertically;
-            fillingStationField.ColumnCount = Settings.MinNumOfCellsHorizontally;
-        }
-
-        private void SetCellsSize()
-        {
-            for (int i = 0; i < fillingStationField.ColumnCount; i++)
-          
-  {
-                fillingStationField.Columns[i].Width = Settings.CellSizeInPx;
-            }
-
-            for (int j = 0; j < fillingStationField.RowCount; j++)
-            {
-                fillingStationField.Rows[j].Height = Settings.CellSizeInPx;
-            }
-        }
-
-        private void SetSpinners()
-        {
-            cellsHorizontally.Minimum = Settings.MinNumOfCellsHorizontally;
-            cellsHorizontally.Maximum = Settings.MaxNumOfCellsHorizontally;
-
-            cellsVertically.Minimum = Settings.MinNumOfCellsVertically;
-            cellsVertically.Maximum = Settings.MaxNumOfCellsVertically;
-
-            cellsHorizontally.Text = fillingStationField.ColumnCount.ToString();
-            cellsVertically.Text = fillingStationField.RowCount.ToString();
-        }
-
         #region события
         private void cellsHorizontally_ValueChanged(object sender, EventArgs e)
         {
---
+++ TopologyConstructorSettings.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+
+namespace software_engineering_2019
+{
+    public partial class TopologyConstructor
+    {
+        private void SetSettings()
+        {
+            SetField();
+            SetCellsSize();
+
+            SetSpinners();
+        }
+
+        private void SetField()
+        {
+            fillingStationField.RowHeadersVisible = false;
+            fillingStationField.ColumnHeadersVisible = false;
+
+            fillingStationField.AllowUserToResizeColumns = false;
+            fillingStationField.AllowUserToResizeRows = false;
+
+            fillingStationField.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            fillingStationField.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            fillingStationField.RowCount = Settings.MinNumOfCellsVertically;
+            fillingStationField.ColumnCount = Settings.MinNumOfCellsHorizontally;
+        }
+
+        private void SetCellsSize()
+        {
+            for (int i = 0; i < fillingStationField.ColumnCount; i++)
+            {
+                fillingStationField.Columns[i].Width = Settings.CellSizeInPx;
+            }
+
+            for (int j = 0; j < fillingStationField.RowCount; j++)
+            {
+                fillingStationField.Rows[j].Height = Settings.CellSizeInPx;
+            }
+        }
+
+        private void SetSpinners()
+        {
+            cellsHorizontally.Minimum = Settings.MinNumOfCellsHorizontally;
+            cellsHorizontally.Maximum = Settings.MaxNumOfCellsHorizontally;
+
+            cellsVertically.Minimum = Settings.MinNumOfCellsVertically;
+            cellsVertically.Maximum = Settings.MaxNumOfCellsVertically;
+
+            cellsHorizontally.Text = fillingStationField.ColumnCount.ToString();
+            cellsVertically.Text = fillingStationField.RowCount.ToString();
+        }
+    }
+}
