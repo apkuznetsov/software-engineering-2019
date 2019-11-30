875cd8e5116afffb97c0183eac7dba8c0383c820
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-11-30 18:02:18 +0400

    Изменен на double тип поля  Price (Цена) сущности Fuel

--- GasStationMs.Dal/Entities/Fuel.cs
+++ GasStationMs.Dal/Entities/Fuel.cs
@@ -8,6 +8,6 @@ namespace GasStationMs.Dal.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
     }
 }
---
+++ GasStationMs.Dal/Migrations/20191130135931_FuelsPriveToDouble.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GasStationMs.Dal.Migrations
+{
+    public partial class FuelsPriveToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Fuels",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Fuels",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
