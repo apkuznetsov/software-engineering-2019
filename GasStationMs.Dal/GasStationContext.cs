1c5f14287ad66a064b66d388a9aabc8122b3a1d1
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-11-30 17:27:25 +0400

    Добавлен DAL слой
    Создан контекст БД (GasStationContext)
    Создана первая миграция (CreateGasStationDb)

---
+++ GasStationMs.Dal/GasStationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace GasStationMs.Dal
+{
+    class GasStationContext : DbContext
+    {
+        public static readonly ILoggerFactory ConsoleLoggerFactory =
+            new LoggerFactory().AddConsole(LogLevel.Information);
+
+        public GasStationContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer("Server=.\\SQLEXPRESS; Initial Catalog=GasStationMs;Integrated Security=True;");
+        }
+    }
+}
