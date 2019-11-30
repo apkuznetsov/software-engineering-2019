f6c3e3036f8398a34bd522a49b5a125ed15a7cca
Andrey <kouznts@ya.ru>
2019-11-30 09:11:35 +0400

    добавить описание топлива

---
+++ AdditionalModels/Fuel.cs
@@ -0,0 +1,20 @@
+﻿namespace software_engineering_2019.AdditionalModels
+{
+    public class Fuel
+    {
+        private string name;
+
+        public Fuel(string name)
+        {
+            this.name = name;
+        }
+
+        public string FuelType
+        {
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
