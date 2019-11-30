214ea4ce95e3d4e0022a860c9ad26e2b34ded6a3
Andrey <kouznts@ya.ru>
2019-11-30 16:41:26 +0400

    описать поле и свойство кассы

--- Models/Cashbox.cs
+++ Models/Cashbox.cs
@@ -1,4 +1,6 @@
-﻿namespace GasStationMs.App.Models
+﻿using System;
+
+namespace GasStationMs.App.Models
 {
     public class Cashbox
     {
@@ -6,5 +8,28 @@
         public static readonly int MaxPricePerLiterOfFuelInRubles = 100;
 
         public static readonly int CashLimitInRubles = 100000;
+
+        private int _moneyInCashInRubles;
+
+        public int MoneyInCashInRubles
+        {
+            get
+            {
+                return _moneyInCashInRubles;
+            }
+
+            set
+            {
+                if (_moneyInCashInRubles < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (_moneyInCashInRubles > CashLimitInRubles)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _moneyInCashInRubles = value;
+            }
+        }
     }
 }
