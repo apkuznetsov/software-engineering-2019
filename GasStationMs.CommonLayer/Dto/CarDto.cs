e038c58b93602a5e06dc09cff6cd0837dc65a5b5
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-08 15:03:06 +0400

    Классы Dto изменены на публичные

--- GasStationMs.CommonLayer/Dto/CarDto.cs
+++ GasStationMs.CommonLayer/Dto/CarDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GasStationMs.CommonLayer.Dto
 {
-    class CarDto
+    public class CarDto
     {
         public int? Id { get; set; }
         public string Name { get; set; }
--- GasStationMs.CommonLayer/Dto/FuelDto.cs
+++ GasStationMs.CommonLayer/Dto/FuelDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GasStationMs.CommonLayer.Dto
 {
-    class FuelDto
+    public class FuelDto
     {
         public int? Id { get; set; }
         public string Name { get; set; }
