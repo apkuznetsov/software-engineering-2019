3c55eafda5745aba52e69f85f32fe508c876c79f
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-08 14:48:12 +0400

    Добавлены Dto для сущностей Car и Fuel

---
+++ GasStationMs.CommonLayer/Dto/FuelDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GasStationMs.CommonLayer.Dto
+{
+    class FuelDto
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
