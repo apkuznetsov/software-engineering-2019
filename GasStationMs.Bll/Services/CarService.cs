7a67d234f117a1eb5b0d2912a8f0b10210df9cd5
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-08 15:04:12 +0400

    Определены методы сервисов и добавлены их пустые реализации

---
+++ GasStationMs.Bll/Services/CarService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GasStationMs.Bll.Services.Interfaces;
+using GasStationMs.CommonLayer.Dto;
+
+namespace GasStationMs.Bll.Services
+{
+    class CarService : ICarService
+    {
+        public IEnumerable<CarDto> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
