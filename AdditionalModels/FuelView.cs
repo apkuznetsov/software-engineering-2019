41424d5561d4a2376fdf1a361835690cf26fde01
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-11 12:47:09 +0400

    Модификация класса FuelView
    Добавлено свойство Id и конструктор, включающий это свойство

--- AdditionalModels/FuelView.cs
+++ AdditionalModels/FuelView.cs
@@ -2,11 +2,19 @@
 {
     public class FuelView
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
 
         public FuelView(string name, double price)
         {
+            Name = name;
+            Price = price;
+        }
+
+        public FuelView(int id, string name, double price)
+        {
+            Id = id;
             Name = name;
             Price = price;
         }
