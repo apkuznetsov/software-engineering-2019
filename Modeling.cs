aeb7cd8ee7bc83a761d58c941db9e390f0690084
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-11 12:22:59 +0400

    Реализован метод  появления машин (SpawnCar)

--- Modeling.cs
+++ Modeling.cs
@@ -12,14 +12,48 @@ namespace GasStationMs.App
 {
     public partial class Modeling : Form
     {
+        private int _timerTicksCount = 0;
+
+        private bool _paused;
+        private readonly Random _rnd = new Random();
+
         public Modeling()
         {
             InitializeComponent();
+
+            this.Controls.Remove(pictureBoxCar);
         }
 
         private void TimerModeling_Tick(object sender, EventArgs e)
         {
+            _timerTicksCount++;
 
+            if (_paused)
+            {
+                return;
+            }
+
+            if (_timerTicksCount > 100)
+            {
+                SpawnCar();
+            }
         }
+
+        #region CarLogic
+
+        private void SpawnCar()
+        {
+            PictureBox car = new PictureBox();
+            car.Tag = "car"; // Will be replaced with CarView class
+            car.Image = Properties.Resources.car_64x34_;
+            car.Left = this.Width - car.Width;
+            car.Top = this.Height - car.Height - 50;
+            car.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            this.Controls.Add(car);
+            //car.BringToFront();
+        }
+
+        #endregion CarLogic
     }
 }
