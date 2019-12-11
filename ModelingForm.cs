4cfccc500d62df6b00154ca379066d6a4dba781f
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-11 14:18:20 +0400

    Грубая реализация метода движения Машины

--- ModelingForm.cs
+++ ModelingForm.cs
@@ -16,6 +16,7 @@ namespace GasStationMs.App
     {
         private int _timerTicksCount = 0;
 
+        private int _carSpeed = 3;
         private bool _paused;
         private readonly Random _rnd = new Random();
 
@@ -36,10 +37,37 @@ namespace GasStationMs.App
                 return;
             }
 
-            if (_timerTicksCount > 100)
+            if (_timerTicksCount % 100 == 0)
             {
                 SpawnCar();
             }
+
+            #region LoopingControls
+
+            foreach (Control c in this.Controls)
+            {
+                if (!(c is PictureBox) || c.Tag == null)
+                {
+                    continue;
+                }
+
+                var pictureBox = (PictureBox)c;
+
+                // Car
+                if (pictureBox.Tag is CarView)
+                {
+                    var car = pictureBox;
+                    var carView = (CarView)car.Tag;
+
+                    //carView.AddDestination()
+
+                    MoveCarToDestination(car);
+
+                    
+                }
+            }
+
+            #endregion /LoopingControls
         }
 
         #region CarLogic
@@ -49,11 +77,13 @@ namespace GasStationMs.App
             var id = _timerTicksCount;
             var name = "mycar";
             var tankVolume = 80;
+            var fuelRemained = 20;
             FuelView fuel = new FuelView(1, "АИ-92", 42.9);
             var isTruck = false;
             var isGoesFilling = true;
 
-            var carView = new CarView(id, name, tankVolume, fuel, isTruck, isGoesFilling);
+            var carView = new CarView(id, name, tankVolume, fuelRemained,
+                fuel, isTruck, isGoesFilling);
 
             PictureBox car = new PictureBox();
             car.Tag = carView; 
@@ -64,6 +94,42 @@ namespace GasStationMs.App
 
             this.Controls.Add(car);
             //car.BringToFront();
+        }
+
+        private void MoveCarToDestination(PictureBox car)
+        {
+            var carView = (CarView)car.Tag;
+
+            //Point destinationPoint = carView.GetDestinationPoint();
+            Point destinationPoint = new Point(100, this.Height - 200);
+
+            // Go left
+            if (car.Left > destinationPoint.X)
+            {
+                car.Left -= _carSpeed;
+                //car.Image = Properties.Resources.car_64x34_left;
+            }
+
+            // Go Right
+            if (car.Right < destinationPoint.X)
+            {
+                car.Left += _carSpeed;
+                //car.Image = Properties.Resources.car_64x34_right;
+            }
+
+            // Go Up
+            if (car.Top > destinationPoint.Y)
+            {
+                car.Top -= _carSpeed;
+                //car.Image = Properties.Resources.car_64x34_up;
+            }
+
+            // Go Down
+            if (car.Bottom < destinationPoint.Y)
+            {
+                car.Top += _carSpeed;
+                //car.Image = Properties.Resources.car_64x34_down;
+            }
         }
 
         #endregion CarLogic
