e0f6e231e9e5c124477f01569c73ab51ba42ef45
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-11 13:16:35 +0400

    Добавлено свойство FuelRemained в класс CarView

--- Modeling/Models/CarView.cs
+++ Modeling/Models/CarView.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int TankVolume { get; set; }
+        public int FuelRemained { get; set; }
 
         public FuelView Fuel { get; set; }
         //public int? FuelTypeId { get; set; }
@@ -13,12 +14,13 @@
         public bool IsTruck { get; set; }
         public bool IsGoesFilling { get; set; }
 
-        public CarView(int id, string name, int tankVolume, FuelView fuelView,
-            bool isTruck, bool isGoesFilling)
+        public CarView(int id, string name, int tankVolume, int fuelRemained,
+            FuelView fuelView, bool isTruck, bool isGoesFilling)
         {
             Id = id;
             Name = name;
             TankVolume = tankVolume;
+            FuelRemained = fuelRemained;
             Fuel = fuelView;
             IsTruck = isTruck;
             IsGoesFilling = isGoesFilling;
