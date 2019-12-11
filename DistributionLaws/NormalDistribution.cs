0997ad125b7573f7c6c08867ecb261eeee5e361d
Andrey <kouznts@ya.ru>
2019-12-11 13:15:05 +0400

    описать нормальный ЗР

---
+++ DistributionLaws/NormalDistribution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GasStationMs.App.DistributionLaws
+{
+    public class NormalDistribution : IDistributionLaw
+    {
+        public double GetRandNumber()
+        {
+            double x = 0;
+
+            Random random = new Random();
+            double y;
+
+            for (int i = 0; i < 12; i++)
+            {
+                y = random.NextDouble();
+                x += y - 6;
+            }
+   
+            return x;
+        }
+    }
+}
