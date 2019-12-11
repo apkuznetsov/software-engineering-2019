40146a85ede11274e8c0f27620e8a23f1cfe4ba6
Andrey <kouznts@ya.ru>
2019-12-11 12:15:36 +0400

    описать равномерный ЗР

---
+++ DistributionLaws/UniformDistribution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GasStationMs.App.DistributionLaws
+{
+    class UniformDistribution : IDistributionLaw
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public UniformDistribution(int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public int MinValue { get; }
+        public int MaxValue { get; }
+
+        public int GetRandomNumber()
+        {
+            Random random = new Random();
+
+            return random.Next(minValue, maxValue + 1);
+        }
+    }
+}
