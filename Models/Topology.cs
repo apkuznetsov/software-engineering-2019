f659582d249dcab64c6c8b13e344ed6339ecc55c
Andrey <kouznts@ya.ru>
2019-12-11 14:12:07 +0400

    описать свойства размера Топологии

--- Models/Topology.cs
+++ Models/Topology.cs
@@ -1,8 +1,10 @@
-﻿namespace GasStationMs.App.Models
+﻿using System;
+
+namespace GasStationMs.App.Models
 {
     public static class Topology
     {
-        public static readonly double ServiceAreaInShare = 0.25;
+        public static readonly double ServiceAreaInShares = 0.25;
 
         #region константы размера
         public static readonly int MinNumOfCellsHorizontally = 10;
@@ -22,13 +24,55 @@
         public static readonly int MinAndMaxNumOfCashboxes = 1;
 
         public static readonly int MinNumberOfFuelTanks = 1;
-        public static int MaxNumberOfFuelTanks
+        public static readonly int MaxNumberOfFuelTanks =
+            (int)(MaxNumOfCellsHorizontally * MaxNumOfCellsVertically * ServiceAreaInShares);
+        #endregion
+
+        private static int numOfCellsHorizontally;
+        private static int numOfCellsVertically;
+
+        public static int NumOfCellsHorizontally
         {
             get
             {
-                return (int)(MaxNumOfCellsHorizontally * MaxNumOfCellsVertically * ServiceAreaInShare);
+                return numOfCellsHorizontally;
+            }
+
+            set
+            {
+                if (value < MinNumOfCellsHorizontally)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value > MaxNumOfCellsHorizontally)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                numOfCellsHorizontally = value;
             }
         }
-        #endregion
+
+        public static int NumOfCellsVertically
+        {
+            get
+            {
+                return numOfCellsVertically;
+            }
+
+            set
+            {
+                if (value < MinNumOfCellsVertically)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value > MaxNumOfCellsVertically)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                numOfCellsVertically = value;
+            }
+        }
     }
 }
