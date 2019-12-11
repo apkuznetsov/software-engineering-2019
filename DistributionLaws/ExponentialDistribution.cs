d7cb69493549c199018d1036ac1ee6e9f1a64529
Andrey <kouznts@ya.ru>
2019-12-11 12:57:00 +0400

    описать экспоненциальный ЗР

---
+++ DistributionLaws/ExponentialDistribution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GasStationMs.App.DistributionLaws
+{
+    public class ExponentialDistribution : IDistributionLaw
+    {
+        private readonly double lambda;
+
+        public ExponentialDistribution(double lambda)
+        {
+            if (lambda <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.lambda = lambda;
+        }
+
+        public int Lambda { get; }
+
+        public double GetRandNumber()
+        {
+            Random random = new Random();
+            double y = random.NextDouble();
+
+            return -(1 / lambda) * Math.Log(y);
+        }
+    }
+}
