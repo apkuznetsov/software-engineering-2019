1a0071ce8aa8377ca7d10a611535b5ce0911655e
Andrey <kouznts@ya.ru>
2019-12-11 12:13:00 +0400

    описать интерфейс законов распределения

---
+++ DistributionLaws/IDistributionLaw.cs
@@ -0,0 +1,11 @@
+﻿namespace GasStationMs.App.DistributionLaws
+{
+    public interface IDistributionLaw
+    {
+        int MinValue { get; }
+
+        int MaxValue { get; }
+
+        int GetRandomNumber();
+    }
+}
