3514e7cf441bac4f03937d9dc61d47590b5b067f
Andrey <kouznts@ya.ru>
2019-12-11 13:49:58 +0400

    рефакторинг Топологии

--- Models/Topology.cs
+++ Models/Topology.cs
@@ -2,20 +2,25 @@
 {
     public static class Topology
     {
+        #region константы размера
         public static readonly int MinNumOfCellsHorizontally = 10;
         public static readonly int MaxNumOfCellsHorizontally = 35;
 
         public static readonly int MinNumOfCellsVertically = 7;
         public static readonly int MaxNumOfCellsVertically = 25;
+        #endregion
+
+        #region константы кол-ва ШЭ
+        public static readonly int MinAndMaxNumOfAdjacentRoads = 1;
+
+        public static readonly int MinAndMaxNumOfEntries = 1;
+        public static readonly int MinAndMaxNumOfExits = 1;
+
+        public static readonly int MinAndMaxNumOfCashboxes = 1;
+        #endregion
 
         public static readonly int ServiceAreaInPercent = 25;
-
         public static readonly int MinNumberOfFuelTanks = 1;
-        // В РАЗРАБОТКЕ, НЕ УДАЛЯТЬ: public static readonly int MaxNumberOfFuelTanks = не более размера служебной области
-
-        public static readonly int NumberOfEntries = 1;
-        public static readonly int NumberOfExits = 1;
-        public static readonly int NumberOfAdjacentRoads = 1;
-        public static readonly int NumberOfCashboxes = 1;
+        //public static readonly int MaxNumberOfFuelTanks = не более размера служебной области
     }
 }
