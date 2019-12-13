d3232cb13a47ba18020125bb351130a08ed78382
Andrey <kouznts@ya.ru>
2019-12-13 21:03:08 +0400

    описать ШЭ въезда

---
+++ TemplateElements/Entry.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace GasStationMs.App.TemplateElements
+{
+    public class Entry
+    {
+        #region изображение
+        private static Bitmap image;
+
+        public static Bitmap Image
+        {
+            get
+            {
+                return image;
+            }
+
+            set
+            {
+                image = value;
+                Icon = Icon.FromHandle(image.GetHicon());
+            }
+        }
+
+        public static Icon Icon { get; private set; }
+        #endregion /изображение
+    }
+}
