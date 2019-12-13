ffb08ad65936c37bb130fc0f9d3be9bf2e634ed1
Andrey <kouznts@ya.ru>
2019-12-13 21:34:28 +0400

    вынести события радиокнопок в отдельный файл

---
+++ Constructor/ConstructorRbs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GasStationMs.App
+{
+    public partial class Constructor
+    {
+        bool isCheckedradioButtonFuelDispenser = false;
+        bool isCheckedradioButtonFuelTank = false;
+
+        private void radioButtonFuelDispenser_CheckedChanged(object sender, EventArgs e)
+        {
+            isCheckedradioButtonFuelDispenser = rbFuelDispenser.Checked;
+        }
+
+        private void radioButtonFuelDispenser_Click(object sender, EventArgs e)
+        {
+            if (rbFuelDispenser.Checked && !isCheckedradioButtonFuelDispenser)
+                rbFuelDispenser.Checked = false;
+            else
+            {
+                rbFuelDispenser.Checked = true;
+                isCheckedradioButtonFuelDispenser = false;
+            }
+        }
+
+        private void radioButtonFuelTank_CheckedChanged(object sender, EventArgs e)
+        {
+            isCheckedradioButtonFuelTank = rbFuelTank.Checked;
+        }
+
+        private void radioButtonFuelTank_Click(object sender, EventArgs e)
+        {
+            if (rbFuelTank.Checked && !isCheckedradioButtonFuelTank)
+                rbFuelTank.Checked = false;
+            else
+            {
+                rbFuelTank.Checked = true;
+                isCheckedradioButtonFuelTank = false;
+            }
+        }
+    }
+}
