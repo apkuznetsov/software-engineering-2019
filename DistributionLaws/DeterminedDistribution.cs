1c5d036e09e5d91fe2c6e16f18faea370712b43a
AListckii <sobbabobba@gmail.com>
2019-12-15 22:35:56 +0400

    добавлен класс детерминированного потока

---
+++ DistributionLaws/DeterminedDistribution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasStationMs.App.DistributionLaws
+{
+    public class DeterminedDistribution : IDistributionLaw
+    {
+        private double time;
+
+        public DeterminedDistribution(double time)
+        {
+            this.time = time;
+        }
+
+        public double GetRandNumber()
+        {            
+            return this.time;
+        }
+    }
+}
