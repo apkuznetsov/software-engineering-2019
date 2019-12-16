d5c8e19eaab79433b5fb43b1977d9eb3f4d07e24
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-16 15:10:48 +0400

    Описаны свойства скорости заправки для FuelDispenserView

--- Modeling/Models/FuelDispenserView.cs
+++ Modeling/Models/FuelDispenserView.cs
@@ -4,15 +4,18 @@
     {
         //public int Id { get; set; }
         public string Name { get; set; }
-        public int SpeedOfFilling { get; set; }
+        public int SpeedOfFillingPerSecond { get; set; }
+        public double SpeedOfFillingPerTick { get; set; }
         public bool IsBusy { get; set; }
         public int CarsInQueue { get; set; }
 
 
-        public FuelDispenserView(string name, int speedOfFilling)
+        public FuelDispenserView(string name, int speedOfFillingPerSecond)
         {
             Name = name;
-            SpeedOfFilling = speedOfFilling;
+            SpeedOfFillingPerSecond = speedOfFillingPerSecond;
+            // Since 20ms is 1 tick, 1second = 1000ms = 50 ticks
+            SpeedOfFillingPerTick = (double) speedOfFillingPerSecond / 50; 
             IsBusy = false;
             CarsInQueue = 0;
         }
