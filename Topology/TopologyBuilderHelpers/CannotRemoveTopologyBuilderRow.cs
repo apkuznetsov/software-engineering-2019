cca95043133325f6c7449502ee6523b4a677586e
Andrey <kouznts@ya.ru>
2019-12-16 11:17:10 +0400

    рефакторинг CannotRemoveTopologyBuilderRow

--- Topology/TopologyBuilderHelpers/CannotRemoveTopologyBuilderRow.cs
+++ Topology/TopologyBuilderHelpers/CannotRemoveTopologyBuilderRow.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GasStationMs.App.Topology.TopologyBuilderHelpers
 {
-    class CannotRemoveTopologyBuilderRow
+    public class CannotRemoveTopologyBuilderRow : Exception
     {
     }
 }
