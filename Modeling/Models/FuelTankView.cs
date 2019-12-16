6bf210645618895a4cd51314496f9dc21f7d3ceb
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-16 18:03:14 +0400

    Добавлено свойство Fuel для FuelTankView

--- Modeling/Models/FuelTankView.cs
+++ Modeling/Models/FuelTankView.cs
@@ -6,13 +6,15 @@
         public string Name { get; set; }
         public int Volume { get; }
         public double CurrentFullness { get; set; }
-        public bool IsEmpty => CurrentFullness > (double)(Volume / 100) * WhenRefillInPercentage;
+        public bool IsEmpty => CurrentFullness < (double)(Volume / 100) * WhenRefillInPercentage;
+        public FuelModel Fuel { get; set; }
 
-        public FuelTankView(string name, int volume)
+        public FuelTankView(string name, int volume, double currentFullness, FuelModel fuel)
         {
             Name = name;
             Volume = volume;
-            CurrentFullness = 0;
+            CurrentFullness = currentFullness;
+            Fuel = fuel;
         }
     }
 }
