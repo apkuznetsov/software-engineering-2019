0aa036de10d89ce769b80b238e20e2fd98c1c316
Andrey <kouznts@ya.ru>
2019-12-17 21:46:13 +0400

    рефакторинг ConstructorRbs.cs

--- Constructor/ConstructorRbs.cs
+++ Constructor/ConstructorRbs.cs
@@ -1,15 +1,15 @@
-﻿using GasStationMs.App.Elements;
+using GasStationMs.App.Elements;
 using System;
 
 namespace GasStationMs.App
 {
     public partial class Constructor
     {
-        bool isCheckedradioButtonFuelDispenser = false;
-        bool isCheckedradioButtonFuelTank = false;
-        bool isCheckedRbCashCounter = false;
-        bool isCheckedRbEntry = false;
-        bool isCheckedRbExit;
+        private bool isCheckedradioButtonFuelDispenser = false;
+        private bool isCheckedradioButtonFuelTank = false;
+        private bool isCheckedRbCashCounter = false;
+        private bool isCheckedRbEntry = false;
+        private bool isCheckedRbExit = false;
 
         private void SetRbsNames()
         {
