b3b1449bfbf1429f77e4f75b8d47b156d4ca10d4
Andrey <kouznts@ya.ru>
2019-12-18 21:39:50 +0400

    описать AreCellsAroundBlankOrDontExist(int x, int y)

--- Topology/TopologyBuilderCashCounter.cs
+++ Topology/TopologyBuilderCashCounter.cs
@@ -1,4 +1,4 @@
-using GasStationMs.App.Elements;
+﻿using GasStationMs.App.Elements;
 using System;
 using System.Drawing;
 using System.Windows.Forms;
@@ -59,8 +59,51 @@ namespace GasStationMs.App.Topology
             return false;
         }
 
+        private bool AreCellsAroundBlankOrDontExist(int x, int y)
+        {
+            Point northWest = new Point(x - 1, y - 1);
+            if (DoesCellExist(northWest.X, northWest.Y))
+                if (!IsCellBlank(northWest.X, northWest.Y))
+                    return false;
 
-        private bool DoesPointExist(int x, int y)
+            Point west = new Point(x - 1, y);
+            if (DoesCellExist(west.X, west.Y))
+                if (!IsCellBlank(west.X, west.Y))
+                    return false;
+
+            Point southWest = new Point(x - 1, y + 1);
+            if (DoesCellExist(southWest.X, southWest.Y))
+                if (!IsCellBlank(southWest.X, southWest.Y))
+                    return false;
+
+            Point north = new Point(x, y - 1);
+            if (DoesCellExist(north.X, north.Y))
+                if (!IsCellBlank(north.X, north.Y))
+                    return false;
+
+            Point south = new Point(x, y + 1);
+            if (DoesCellExist(south.X, south.Y))
+                if (!IsCellBlank(south.X, south.Y))
+                    return false;
+
+            Point northEast = new Point(x + 1, y - 1);
+            if (DoesCellExist(northEast.X, northEast.Y))
+                if (!IsCellBlank(northEast.X, northEast.Y))
+                    return false;
+
+            Point east = new Point(x + 1, y);
+            if (DoesCellExist(east.X, east.Y))
+                if (!IsCellBlank(east.X, east.Y))
+                    return false;
+
+            Point southEast = new Point(x + 1, y + 1);
+            if (DoesCellExist(southEast.X, southEast.Y))
+                if (!IsCellBlank(southEast.X, southEast.Y))
+                    return false;
+
+            return false;
+        }
+
         private bool DoesCellExist(int x, int y)
         {
             bool isExLess = x < 0;
