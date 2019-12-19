8fa4af8d4b865544730a108d975b066bd11a0ae0
Andrey <kouznts@ya.ru>
2019-12-19 18:15:46 +0400

    добавить ярлыки

--- Start.Designer.cs
+++ Start.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.labelAbout = new System.Windows.Forms.Label();
+            this.labelDoSomething = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(239, 129);
+            this.button1.Location = new System.Drawing.Point(239, 243);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 24);
@@ -55,12 +57,34 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // labelAbout
+            // 
+            this.labelAbout.AutoSize = true;
+            this.labelAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAbout.Location = new System.Drawing.Point(31, 62);
+            this.labelAbout.Name = "labelAbout";
+            this.labelAbout.Size = new System.Drawing.Size(528, 33);
+            this.labelAbout.TabIndex = 2;
+            this.labelAbout.Text = "Система моделирования работы АЗС";
+            // 
+            // labelDoSomething
+            // 
+            this.labelDoSomething.AutoSize = true;
+            this.labelDoSomething.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDoSomething.Location = new System.Drawing.Point(73, 132);
+            this.labelDoSomething.Name = "labelDoSomething";
+            this.labelDoSomething.Size = new System.Drawing.Size(464, 24);
+            this.labelDoSomething.TabIndex = 3;
+            this.labelDoSomething.Text = "Создайте топологию АЗС или загрузите из файла";
+            // 
             // formStart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.labelDoSomething);
+            this.Controls.Add(this.labelAbout);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -69,6 +93,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Начало работы";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,5 +101,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label labelAbout;
+        private System.Windows.Forms.Label labelDoSomething;
     }
 }
