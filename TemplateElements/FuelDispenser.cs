3cba89ba2fddc9ac7c349c1cdf22deffb0e4616d
Andrey <kouznts@ya.ru>
2019-12-19 21:51:45 +0400

    описать св-во скорости подачи топлива ТРК

--- TemplateElements/FuelDispenser.cs
+++ TemplateElements/FuelDispenser.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Drawing;
 
 namespace GasStationMs.App.Elements
@@ -30,7 +30,30 @@ namespace GasStationMs.App.Elements
         public static readonly int MinFuelFeedRateInLitersPerMinute = 25;
         public static readonly int MaxFuelFeedRateInLitersPerMinute = 160;
         #endregion /статика
-        
+
+        private int fuelFeedRateInLitersPerMinute = 25;
+
+        public FuelDispenser()
+        {
+            fuelFeedRateInLitersPerMinute = MinFuelFeedRateInLitersPerMinute;
+        }
+
+        public int FuelFeedRateInLitersPerMinute
+        {
+            get
+            {
+                return fuelFeedRateInLitersPerMinute;
+            }
+
+            set
+            {
+                if (value < MinFuelFeedRateInLitersPerMinute)
+                    throw new ArgumentOutOfRangeException();
+
+                fuelFeedRateInLitersPerMinute = value;
+            }
+        }
+
         public override string ToString()
         {
             return
