b17adc64febe2d6fc8623990f29ef4eba97ac0fc
Andrey <kouznts@ya.ru>
2019-12-19 19:33:22 +0400

    добавить кнопку Загрузить

--- Start.Designer.cs
+++ Start.Designer.cs
@@ -32,6 +32,7 @@
             this.btnCancel = new System.Windows.Forms.Button();
             this.labelAbout = new System.Windows.Forms.Label();
             this.labelDoSomething = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateTopology
@@ -77,12 +78,23 @@
             this.labelDoSomething.TabIndex = 3;
             this.labelDoSomething.Text = "Создайте топологию АЗС или загрузите из файла";
             // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(216, 194);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(127, 23);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Загрузить";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
             // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnDownload);
             this.Controls.Add(this.labelDoSomething);
             this.Controls.Add(this.labelAbout);
             this.Controls.Add(this.btnCancel);
@@ -103,5 +115,6 @@
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Label labelAbout;
         private System.Windows.Forms.Label labelDoSomething;
+        private System.Windows.Forms.Button btnDownload;
     }
 }
