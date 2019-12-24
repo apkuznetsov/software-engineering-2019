76d1df1757085819ad58cb50d720c3cee27d9c5a
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-25 01:37:38 +0400

    Add Car and Collector PictureBox classes

---
+++ Modeling/Models/PictureBoxes/CarPictureBox.cs
@@ -0,0 +1,24 @@
+﻿using GasStationMs.App.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace GasStationMs.App.Modeling.Models.PictureBoxes
+{
+    internal class CarPictureBox : MoveablePictureBox
+    {
+        public CarPictureBox(ModelingForm modelingForm, CarView carView)
+        {
+            Tag = carView;
+            Image = Properties.Resources.car_32x17__left;
+            Location = DestinationPointsDefiner.SpawnPoint;
+            SizeMode = PictureBoxSizeMode.AutoSize;
+
+            IsGoesFilling = false;
+
+            MouseClick += new MouseEventHandler(ClickEventProvider.CarPictureBox_Click);
+
+            modelingForm.PlaygroundPanel.Controls.Add(this);
+            BringToFront();
+        }
+    }
+}
