c1f82b054e130e00bc9ecb15179ac31e6cd01a10
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-24 12:58:21 +0400

    Add class MappedTopology

---
+++ Modeling/MappedTopology.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GasStationMs.App.Modeling
+{
+    internal class MappedTopology
+    {
+        internal PictureBox CashCounter { get; set; }
+        internal PictureBox Enter { get; set; }
+        internal PictureBox Exit { get; set; }
+        internal List<PictureBox> FuelDispensersList { get; set; }
+        internal List<PictureBox> FuelTanksList { get; set; }
+
+        internal Dictionary<PictureBox, Point> FuelDispensersDestPoints { get; }
+
+        internal MappedTopology()
+        {
+            FuelDispensersList = new List<PictureBox>();
+            FuelTanksList = new List<PictureBox>();
+            FuelDispensersDestPoints = new Dictionary<PictureBox, Point>();
+        }
+
+        internal void AddFuelDispenserWithDestPoint(PictureBox fuelDispenser, Point pointOfFueling)
+        {
+            FuelDispensersDestPoints.Add(fuelDispenser, pointOfFueling);
+        }
+    }
+}
