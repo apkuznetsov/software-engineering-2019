05362c60788518572402966d9f95cd80c2370494
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-24 12:55:37 +0400

    Add class CarCreator

---
+++ Modeling/CarCreator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static GasStationMs.App.Modeling.DestinationPointsDefiner;
+
+namespace GasStationMs.App.Modeling
+{
+    internal static class CarCreator
+    {
+        // test
+        private static Random _rnd = new Random();
+        // /test
+       
+        internal static void SpawnCar( /*CarModel carModel*/)
+        {
+            var carView = ElementViewProducer.CreateCarView( /*caeModel*/);
+
+            // Some Distribution law here
+            if (_rnd.NextDouble() >= 0.5)
+            {
+                carView.IsGoesFilling = true;
+            }
+            //carView.IsGoesFilling = true;
+
+
+            if (!carView.IsGoesFilling)
+            {
+                carView.AddDestinationPoint(LeavePointNoFilling);
+            }
+
+            ElementPictureBoxProducer.CreateCarPictureBox(carView);
+        }
+    }
+}
