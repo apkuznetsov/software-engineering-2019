5b4b0987170d9b0665b8a7f6044c715cd4b088e3
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-26 01:38:52 +0400

    Add RefuellerCreator class

---
+++ Modeling/MovingLogic/Refueller/RefuellerCreator.cs
@@ -0,0 +1,15 @@
+﻿using GasStationMs.App.Modeling.Models.Views;
+using GasStationMs.App.TemplateElements;
+
+namespace GasStationMs.App.Modeling.MovingLogic.Refueller
+{
+    internal static class RefuellerCreator
+    {
+        internal static void SpawnRefueller(FuelTankView fuelTank)
+        {
+            var refuellerView = ElementViewProducer.CreateRefuellerView(fuelTank);
+            var refueller = ElementPictureBoxProducer.CreateRefuellerPictureBox(refuellerView);
+        }
+    }
+}
+
