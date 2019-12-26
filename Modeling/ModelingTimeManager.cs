ffacd11677e1c892a6bd8d60d0702dad94e37bcd
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-26 20:41:04 +0400

    Add IsPaused property to the TImeManager class

--- Modeling/ModelingTimeManager.cs
+++ Modeling/ModelingTimeManager.cs
@@ -3,10 +3,13 @@
     internal static class ModelingTimeManager
     {
         internal const int MillisecondsForTimerTick = 20;
+        internal static bool IsPaused { get; set; }
+
 
         internal static void SetUpModelingTimeManager(System.Windows.Forms.Timer timerModeling)
         {
             timerModeling.Interval = MillisecondsForTimerTick;
+            IsPaused = false;
         }
     }
 }
