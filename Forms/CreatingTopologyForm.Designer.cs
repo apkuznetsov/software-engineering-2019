27a9887d287a50a4288c3f8a52013cf336be844d
Andrey <kouznts@ya.ru>
2019-12-27 00:32:30 +0400

    запретить изменять размер формы

--- Forms/CreatingTopologyForm.Designer.cs
+++ Forms/CreatingTopologyForm.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CreatingTopologyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CreatingTopologyForm";
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CreatingTopologyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Создание топологии";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
