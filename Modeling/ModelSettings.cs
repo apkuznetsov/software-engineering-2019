823bfee16bdee94994194d31336d5a410486a318
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-26 18:58:07 +0400

    Add ModelSettings class

---
+++ Modeling/ModelSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using GasStationMs.App.DB.Models;
+using GasStationMs.App.DistributionLaws;
+
+namespace GasStationMs.App.Modeling
+{
+    internal static class ModelSettings
+    {
+        internal static List<FuelModel> Fuels { get;} = new List<FuelModel>();
+        internal static IDistributionLaw TimeBetweenCarsGenerator { get; set; }
+
+        internal static void AddUniqueFuel(FuelModel fuel)
+        {
+            if (!Fuels.Contains(fuel))
+            {
+                Fuels.Add(fuel);
+            }
+        }
+    }
+}
