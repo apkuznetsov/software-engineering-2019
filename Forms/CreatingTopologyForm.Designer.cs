4423c88c5b5f10a3ffd457eaa1eb056968bedb4c
Andrey <kouznts@ya.ru>
2019-12-27 00:45:46 +0400

    запоминать расположение файла

--- Forms/CreatingTopologyForm.Designer.cs
+++ Forms/CreatingTopologyForm.Designer.cs
@@ -28,22 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelChooseFilePath = new System.Windows.Forms.Label();
+            this.btnFilePath = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelChooseFilePath
+            // 
+            this.labelChooseFilePath.AutoSize = true;
+            this.labelChooseFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelChooseFilePath.Location = new System.Drawing.Point(12, 79);
+            this.labelChooseFilePath.Name = "labelChooseFilePath";
+            this.labelChooseFilePath.Size = new System.Drawing.Size(426, 25);
+            this.labelChooseFilePath.TabIndex = 0;
+            this.labelChooseFilePath.Text = "Задайте расположение файла топологии";
+            // 
+            // btnFilePath
+            // 
+            this.btnFilePath.Location = new System.Drawing.Point(494, 83);
+            this.btnFilePath.Name = "btnFilePath";
+            this.btnFilePath.Size = new System.Drawing.Size(78, 23);
+            this.btnFilePath.TabIndex = 1;
+            this.btnFilePath.Text = "Обзор";
+            this.btnFilePath.UseVisualStyleBackColor = true;
+            this.btnFilePath.Click += new System.EventHandler(this.btnFilePath_Click);
             // 
             // CreatingTopologyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnFilePath);
+            this.Controls.Add(this.labelChooseFilePath);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CreatingTopologyForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Создание топологии";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelChooseFilePath;
+        private System.Windows.Forms.Button btnFilePath;
     }
 }
--- Forms/CreatingTopologyForm.cs
+++ Forms/CreatingTopologyForm.cs
@@ -12,9 +12,28 @@ namespace GasStationMs.App.Forms
 {
     public partial class CreatingTopologyForm : Form
     {
+        private string currFilePath;
+
         public CreatingTopologyForm()
         {
             InitializeComponent();
+        }
+
+        private void btnFilePath_Click(object sender, EventArgs e)
+        {
+            string dotExt = Topology.Topology.DotExt;
+            string filter = " " + dotExt + "|" + "*" + dotExt;
+
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                DefaultExt = dotExt,
+                Filter = filter
+            };
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                currFilePath = sfd.FileName;
+            }
         }
     }
 }
